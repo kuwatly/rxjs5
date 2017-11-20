7d4a48292ddb5e30cffb0ccedb9916b7ff637404
Iyad Kuwatly <ikuwatly@gmail.com>
2017-11-19 21:50:59 -0500

    Read json file using xhr

--- src/main.ts
+++ src/main.ts
@@ -43,3 +43,30 @@ sourceEvent.subscribe(
     e => console.log(`error: ${e}`),
     () => console.log(`complete`)
 );
+
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+let clickEvent = Observable.fromEvent(button, "click");
+
+function load(url: string) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("load", () => {
+        let movies = JSON.parse(xhr.responseText);
+        movies.forEach( m => {
+            let div = document.createElement("div");
+            div.innerText = m.title;
+            output.appendChild(div);
+        })
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+clickEvent.subscribe(
+    e => load("movies.json"),
+    e => console.log(`error: ${e}`),
+    () => console.log(`complete`)
+);
